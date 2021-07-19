1cdea656443f89354d4bbcc84795badfe6f4a5de
Vincent Chooi <vinceecws@gmail.com>
2021-07-19 18:41:26 -0400

    Implemented weather reports for all frequencies

--- src/main/scala/p0.scala
+++ src/main/scala/p0.scala
@@ -1,6 +1,8 @@
 import scala.util.parsing.json._
 import scala.io.Source._
-
+import Console._
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class Weather(weather: Map[String, Any], freq: String) {
 
@@ -18,11 +20,19 @@ class Weather(weather: Map[String, Any], freq: String) {
         lines.close
     }
 
+    def printReport(withIcon: Boolean=true) = {
+        if (withIcon) {
+            printWeatherIcon(icon)
+        }
+
+        println(s"Weather: ${main}")
+        println(s"${description}")
+    }
 }
 
-class Current(current: Map[String, Any]) {
+class Current(current: Map[String, Any], city: String, state: String, timezone_offset: Int) {
 
-    val dt = current("dt").asInstanceOf[Double].toInt
+    val dt = current("dt").asInstanceOf[Double].toLong
     val sunrise = current("sunrise").asInstanceOf[Double].toInt
     val sunset = current("sunset").asInstanceOf[Double].toInt
     val temp = current("temp").asInstanceOf[Double].toFloat
@@ -38,15 +48,28 @@ class Current(current: Map[String, Any]) {
     val weather = new Weather(current("weather").asInstanceOf[List[Map[String, Any]]](0), "current")
     val rain = current.getOrElse("rain", null)
     val snow = current.getOrElse("snow", null)
+
+    def printReport() = {
+        val date = new SimpleDateFormat("EEE, d MMM yyyy, h:mm a").format(new Date ((dt - timezone_offset) * 1000))
+        println(date)
+        println(s"${city}, ${state} \n")
+
+        println("Current Weather: ")
+        weather.printReport(withIcon=false)
+        println(s"Temperature: ${temp}")
+        println(s"Feels Like: ${feels_like}")
+        println(s"Visibility: ${visibility}")
+        println(s"Humidity: ${humidity}\n\n")
+    }
 }
 
 // Minutely data forecast for the next 60 minutes
-class Minutely(minutely: List[Map[String, Any]]) {
+class Minutely(minutely: List[Map[String, Any]], city: String, state: String, dt_now: Long, timezone_offset: Int) {
 
     class Minute(minute: Map[String, Any], ind: Int) {
 
         val index = ind
-        val dt = minute("dt").asInstanceOf[Double].toInt
+        val dt = minute("dt").asInstanceOf[Double].toLong
         val precipitation = minute("precipitation").asInstanceOf[Double].toFloat
     }
 
@@ -55,15 +78,26 @@ class Minutely(minutely: List[Map[String, Any]]) {
     def atMinute(minute: Int): Minute = {
         return _data(minute)
     }
+
+    def printReport() = {
+        val date = new SimpleDateFormat("EEE, d MMM yyyy").format(new Date ((dt_now + timezone_offset) * 1000))
+        println(date)
+        println(s"${city}, ${state} \n")
+
+        println("Precipitation for the next hour: ")
+        for (min <- _data) {
+            println(s"${new SimpleDateFormat("h:mm a").format(new Date ((min.dt + timezone_offset) * 1000))} ${min.precipitation}\n")
+        }
+    }
 }
 
-// Hourly data forecast for the next 48 hours
-class Hourly(hourly: List[Map[String, Any]]) {
+// Hourly data forecast for the next 24 hours
+class Hourly(hourly: List[Map[String, Any]], city: String, state: String, dt_now: Long, timezone_offset: Int) {
 
     class Hour(hour: Map[String, Any], ind: Int) {
 
         val index = ind
-        val dt = hour("dt").asInstanceOf[Double].toInt
+        val dt = hour("dt").asInstanceOf[Double].toLong
         val temp = hour("temp").asInstanceOf[Double].toFloat
         val feels_like = hour("feels_like").asInstanceOf[Double].toFloat
         val pressure = hour("pressure").asInstanceOf[Double].toFloat
@@ -86,16 +120,33 @@ class Hourly(hourly: List[Map[String, Any]]) {
     def atHour(hour: Int): Hour = {
         return _data(hour)
     }
+
+    def printReport() = {
+        val date = new SimpleDateFormat("h:mm a").format(new Date ((dt_now + timezone_offset) * 1000))
+        println(date)
+        println(s"${city}, ${state} \n")
+        println("Weather for the next 24 hours: ")
+
+        println("Now")
+        atHour(0).weather.printReport(withIcon=false)
+        println(s"Temperature: ${atHour(0).temp}\n\n")
+
+        for (hour <- _data.slice(1, 24)) {
+            println(s"${new SimpleDateFormat("h a").format(new Date ((hour.dt + timezone_offset) * 1000))}")
+            hour.weather.printReport(withIcon=false)
+            println(s"Temperature: ${hour.temp}\n\n")
+        }
+    }
 }
 
 // Daily data forecast for the next 7 days
-class Daily(daily: List[Map[String, Any]]) {
+class Daily(daily: List[Map[String, Any]], city: String, state: String, dt_now: Long, timezone_offset: Int) {
 
     class Day(day: Map[String, Any], ind: Int) {
 
         val index = ind
-        val dt = day("dt").asInstanceOf[Double].toInt
-        val temp = day("temp")
+        val dt = day("dt").asInstanceOf[Double].toLong
+        val temp = day("temp").asInstanceOf[Map[String, Double]]
         val feels_like = day("feels_like")
         val pressure = day("pressure").asInstanceOf[Double].toFloat
         val humidity = day("humidity").asInstanceOf[Double].toFloat
@@ -116,10 +167,26 @@ class Daily(daily: List[Map[String, Any]]) {
     def atDay(day: Int): Day = {
         return _data(day)
     }
+
+    def printReport() = {
+        val date = new SimpleDateFormat("EEE, d MMM yyyy, h:mm a").format(new Date ((dt_now + timezone_offset) * 1000))
+        println(date)
+        println(s"${city}, ${state} \n")
+
+        println("Weather for the next 7 days: ")
+        for (day <- _data) {
+            val date = new SimpleDateFormat("EEE, d MMM yyyy").format(new Date ((day.dt + timezone_offset) * 1000))
+            println(date)
+            day.weather.printReport(withIcon=false)
+            println("Temperature: ")
+            println(s"Morning: ${day.temp("morn")} -> Day: ${day.temp("day")} -> Evening: ${day.temp("eve")} -> Night: ${day.temp("night")}")
+            println(s"Lowest: ${day.temp("min")}, Highest: ${day.temp("max")}\n\n")
+        }
+    }
 }
 
 // Current weather alerts, if available
-class Alerts(alerts: List[Map[String, Any]]) {
+class Alerts(alerts: List[Map[String, Any]], city: String, state: String, dt_now: Long, timezone_offset: Int) {
 
     class Alert(alert: Map[String, Any], ind: Int) {
 
@@ -138,18 +205,18 @@ class Alerts(alerts: List[Map[String, Any]]) {
     }
 }
 
-class WeatherData(weatherRes: Map[String, Any]) {
+class WeatherData(weatherRes: Map[String, Any], city: String, state: String) {
 
     val lat_lon = (weatherRes("lat").asInstanceOf[Double].toFloat, weatherRes("lon").asInstanceOf[Double].toFloat)
     val timezone = weatherRes("timezone").asInstanceOf[String]
     val timezone_offset = weatherRes("timezone_offset").asInstanceOf[Double].toInt
-    val current = new Current(weatherRes("current").asInstanceOf[Map[String, Any]])
-    val minutely = new Minutely(weatherRes("minutely").asInstanceOf[List[Map[String, Any]]])
-    val hourly = new Hourly(weatherRes("hourly").asInstanceOf[List[Map[String, Any]]])
-    val daily = new Daily(weatherRes("daily").asInstanceOf[List[Map[String, Any]]])
+    val current = new Current(weatherRes("current").asInstanceOf[Map[String, Any]], city, state, timezone_offset)
+    val minutely = new Minutely(weatherRes("minutely").asInstanceOf[List[Map[String, Any]]], city, state, current.dt, timezone_offset)
+    val hourly = new Hourly(weatherRes("hourly").asInstanceOf[List[Map[String, Any]]], city, state, current.dt, timezone_offset)
+    val daily = new Daily(weatherRes("daily").asInstanceOf[List[Map[String, Any]]], city, state, current.dt, timezone_offset)
     var alerts = weatherRes.getOrElse("alerts", null)
     if (alerts != null) {
-        alerts = new Alerts(weatherRes("alerts").asInstanceOf[List[Map[String, Any]]])
+        alerts = new Alerts(weatherRes("alerts").asInstanceOf[List[Map[String, Any]]], city, state, current.dt, timezone_offset)
     }
 }
 
@@ -157,6 +224,7 @@ object p0 {
 
     val apiKey = "92918be308104905debec438abdf41b0"
     val excludeOpts = Set("current", "minutely", "hourly", "daily", "alerts")
+    val units = "imperial"
 
     def getGeo(city: String, state: String, limit: Int = 10): Map[String, String] = {
         val url = s"http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=${limit}&appid=${apiKey}"
@@ -182,7 +250,7 @@ object p0 {
     def getWeather(lat_lon: Map[String, String], exclude: String = null): Map[String, Any] = {
         if (exclude != null && !excludeOpts(exclude)) throw new IllegalArgumentException("Invalid argument for arg except")
 
-        val url = s"https://api.openweathermap.org/data/2.5/onecall?lat=${lat_lon("lat")}&lon=${lat_lon("lon")}&exclude=${exclude}&appid=${apiKey}"
+        val url = s"https://api.openweathermap.org/data/2.5/onecall?lat=${lat_lon("lat")}&lon=${lat_lon("lon")}&exclude=${exclude}&appid=${apiKey}&units=${units}"
         val res = JSON.parseFull(fromURL(url).mkString) match {
             case Some(res) => res.asInstanceOf[Map[String, Any]]
             case _ => Map[String, Any]()
@@ -191,8 +259,28 @@ object p0 {
         return res
     }
 
+    def extractInput(input: String): (String, String) = {
+        val reg = "[A-Za-z -]+,[A-Za-z ]+".r
+        val res = reg.findFirstIn(input) match {
+            case Some(res) => res.asInstanceOf[List[String]]
+            case _ => List[String](",")
+        }
+
+        val cityState = res(0).split(",")
+
+        return (cityState(0), cityState(1))
+    }
+
     def main(args: Array[String]): Unit = {
+        println("")
+        // val place = readLine(s"${BOLD}Which city's weather report would you like to see? Input as: city, state ${RESET}")
+        // val cityState = extractInput(place)
+        // val lat_lon = getGeo(cityState._1, cityState._2)
         val lat_lon = Map("lon" -> "-73.7976", "lat" -> "40.7498")
-        val weatherData = new WeatherData(getWeather(lat_lon))
+        val weatherData = new WeatherData(getWeather(lat_lon), "Queens", "NY")
+        weatherData.current.printReport()
+        weatherData.minutely.printReport()
+        weatherData.hourly.printReport()
+        weatherData.daily.printReport()
     }
 }
